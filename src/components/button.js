d4eed50abf54bfa5305681a3b370700ab863af19
chenjianan <chenjianan@kingsoft.com>
2016-02-15 17:32:38 +0800

    btn function

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,32 @@
 define(['node_modules/kpc/src/views/components/button'], function(template) {
     return Intact.extend({
         defaults: {
+            type:'white',
+            className:'',
+            href:'',
+            attributes:'',
+            icon:'',
+            text:'这是一个按钮'
 
         },
 
-        template: template
+        template: template,
+
+        _create: function() {
+            var self = this;
+            //$(this.element).find('select')
+        },
+
+        _update: function() {
+
+        },
+
+        btnDisable:function(){
+            this.set("className","c-button-disabled");
+        },
+
+        btnEnable:function(){
+            this.set("className"," ");
+        }
     });
 });
