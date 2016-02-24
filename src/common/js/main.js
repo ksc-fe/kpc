1f828b074d408222368e38c5c5f3cfe9436a7c2b
Javey <jiawei23716@sina.com>
2016-02-24 14:36:32 +0800

    javey

--- src/common/js/main.js
+++ src/common/js/main.js
@@ -3,9 +3,19 @@ Vdt.setDelimiters(['{{', '}}']);
 require.config({
     baseUrl: '/',
 
+    paths: {
+        jquery: 'node_modules/kpc/src/common/js/lib/jquery'
+    },
     shim: {
         director: {
             exports: 'Router'
+        },
+        jquery: {
+            exports: 'jQuery'
         }
     }
+});
+
+define('jquery', [], function() {
+    return jQuery;
 });
