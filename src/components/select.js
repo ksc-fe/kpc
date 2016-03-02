5f22936937caf011c7942eb81122f697a31c56a0
huangzj <huangzj08@gmail.com>
2016-03-02 15:50:04 +0800

    select

--- src/components/select.js
+++ src/components/select.js
@@ -6,17 +6,20 @@ define(['node_modules/kpc/src/views/components/select'], function(template) {
             disabled: false,
             className: '',
             name: undefined,
-            width: 200,
-            parent: null,
-            model: null
+            width: 200
         },
 
         template: template,
 
         _create: function() {
             var self = this;
+            var appendTo = self.get('appendTo');
             $(this.element).find('select')
-                .selectmenu({width: this.get('width')})
+                .selectmenu({
+                    width: this.get('width'),
+                    appendTo: appendTo === 'self' ? this.element :appendTo,
+                    position: self.get('position')
+                })
                 .on('selectmenuchange', function(e, ui) {
                     self.set('value', ui.item.value, {silent: true});
                     self.trigger('change', e, ui);
