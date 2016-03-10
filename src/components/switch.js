0322113841734953ac0e4dbc99cbbd609d05adda
Javey <jiawei23716@sina.com>
2016-03-10 17:00:56 +0800

    javey

---
+++ src/components/switch.js
@@ -0,0 +1,33 @@
+define(['node_modules/kpc/src/common/js/lib/bootstrap-switch'], function() {
+    return Intact.extend({
+        defaults: {
+            state: true,
+            name: undefined,
+            size: 'mini',
+            onText: undefined,
+            offText: undefined,
+            disabled: false,
+            readonly: false 
+        },
+
+        template: Vdt.compile('<div><input type="checkbox" name={{ self.get("name") }}/></div>'),
+
+        _init: function() {
+            this.on('change:state', function(component, state) {
+                this.method('state', state);
+            });
+        },
+
+        _create: function() {
+            var self = this;
+            this.$input = $(this.element).find('input').bootstrapSwitch(this.get())
+                .on('switchChange.bootstrapSwitch', function(e, state) {
+                    self.trigger('changed', e, state); 
+                });
+        },
+
+        method: function() {
+            this.$input.bootstrapSwitch.apply(this.$input, arguments);
+        }
+    });
+});
