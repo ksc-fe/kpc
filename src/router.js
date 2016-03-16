8e5d6ad81d6e4f938121d62274207cca2c40884a
Javey <jiawei23716@sina.com>
2016-03-16 16:14:01 +0800

    javey

--- src/router.js
+++ src/router.js
@@ -3,11 +3,9 @@ var Router = require(__ROOT + '/node_modules/advanced/lib/index').Router;
 
 // 所有页面请求都去拿用户信息
 // 排除静态资源
-router.get(/^(?!(\/static\/|\/res\/|\/node_modules\/)).*$/, runCtrl('./middlewares/user', 'userInfo'));
+router.get(/^(?!(\/static\/|\/res\/|\/node_modules\/)).*$/, runCtrl('./middlewares/user', 'userInfo'), runCtrl('./middlewares/sidebar', 'getNavData'));
 // 所有/console页面都需要登录访问
 router.get('/console/*', runCtrl('./middlewares/user', 'needLogin'));
-router.get('/console/*', runCtrl('./middlewares/sidebar', 'getNavData'));
-
 
 function runCtrl(path, method) {
     var Ctrl = require(path);
