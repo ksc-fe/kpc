076ac249ea878fae3c5b01040eaa28e325809a9b
Javey <jiawei23716@sina.com>
2016-03-17 15:20:47 +0800

    javey

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -8,4 +8,16 @@ require(['node_modules/kpc/src/common/js/widgets/popmenu'], function() {
             collision: "none"
         }
     });
+    
+    // 针对视频云这类能展开的菜单，特殊处理
+    $(document).on('click', '#sidebar .expand', function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.toggleClass('active');
+        var $icon = $this.find('.icon'),
+            className = $icon.attr('class'),
+            classNames = className.match(/c\-icon\-(\w+)/);
+        $this.find('.icon').toggleClass('c-icon-' + classNames[1] + ' c-icon-' + classNames[1] + '-hover');
+        $this.find('.arrow').toggleClass('c-icon-nav-arrow-right c-icon-nav-arrow-down-hover');
+    });
 });
--- src/middlewares/sidebar.js
+++ src/middlewares/sidebar.js
@@ -1,131 +1,12 @@
 var Advanced = require(__ROOT + '/node_modules/advanced/lib/index');
 
-
 module.exports = Advanced.Controller.extend({
 
-    getNavData: function(){
+    getNavData: function() {
 
         if (this.req.xhr) return this.next();
 
-        var navData = [
-            {
-                "title": "计算资源",
-                "icon": "compute",
-                'href': '/console/vm/instance/list',
-                "subNav": [
-                    {
-                        "title": "云主机",
-                        "href": "/console/vm/instance/list",
-                        "index": "kvm"
-                    },
-                    {
-                        "title": "镜像",
-                        "href": "/console/snapshot/list",
-                        "index": "image"
-                    },
-                    {
-                        "title": "防火墙",
-                        "href": "/console/firewall/list",
-                        "index": "firewall"
-                    }
-                ]
-            },
-            {
-                "title": "网络资源",
-                "icon": "network",
-                'href': '/console/eip/list',
-                "subNav": [
-                    {
-                        "title": "弹性IP",
-                        "href": "/console/eip/list",
-                        "index": "eip"
-                    },
-                    {
-                        "title": "负载均衡",
-                        "href": "/console/lb/instance/list",
-                        "index": "lbs"
-                    },
-                    {
-                        "title": "VPC",
-                        "href": "/console/vpc/list",
-                        "index":"vpc"
-                    },
-                    {
-                        "title": "共享带宽",
-                        "href": "/console/bandwidth/list",
-                        "index":"bandwidth"
-                    }
-                ]
-            },
-            {
-                "title": "数据库",
-                "icon": "rds",
-                'href': '/console/rds/instances/list',
-                "subNav": [
-                    {
-                        "title": "关系型数据库",
-                        "href": "/console/rds/instances/list",
-                        "index": "rds"
-                    },
-                    {
-                        "title": "缓存",
-                        "href": "/console/kcs/list",
-                        "index": "kcs"
-                    }
-                ]
-            },
-            {
-                "title": "CDN",
-                "icon": "cdn",
-                "href": "/console/cdn/domain/list",
-                "subNav": [
-                    {
-                        "title": "加速管理",
-                        "href": "/console/cdn/domain/list",
-                        "index": "domain"
-                    },
-                    {
-                        "title": "日志下载",
-                        "href": "/console/cdn/flow/getLogs",
-                        "index": "log"
-                    }
-                ]
-            },
-            {
-                "title": "云硬盘",
-                "icon": "ebs",
-                "href": "/console/ebs/instances/list",
-                "subNav": [
-                    {
-                        "title": "数据盘",
-                        "href": "/console/ebs/instances/list",
-                        "index": "disk"
-                    },
-                    {
-                        "title": "快照",
-                        "href": "/console/ebs/instances/snapshots",
-                        "index": "snapshots"
-                    }
-                ]
-            },
-            {
-                "title": "云安全",
-                "icon": "safe",
-                'href': '/console/security/basic',
-                "subNav": [
-                    {
-                        "title": "基础防护",
-                        "href": "/console/security/basic",
-                        "index": "security-basic"
-                    },
-                    {
-                        "title": "高防IP",
-                        "href": "/console/security/kad",
-                        "index": "security-kad"
-                    }
-                ]
-            }
-        ];
+        var navData = require('../data/nav.json');
 
         if(Advanced.Utils.isDev()){
             navData = [
@@ -196,6 +77,23 @@ module.exports = Advanced.Controller.extend({
                     ]
                 },
                 {
+                    "title": "安全",
+                    "icon": "safe",
+                    'href': '/console/security/basic',
+                    "subNav": [
+                        {
+                            "title": "基础防护",
+                            "href": "/console/security/basic",
+                            "index": "security-basic"
+                        },
+                        {
+                            "title": "高防IP",
+                            "href": "/console/security/kad",
+                            "index": "security-kad"
+                        }
+                    ]
+                },
+                {
                     "title": "CDN",
                     "icon": "cdn",
                     "href": "/console/cdn/domain/list",
@@ -243,23 +141,6 @@ module.exports = Advanced.Controller.extend({
                             "title": "联系人管理",
                             "href": "#/linkman",
                             "index": "linkman"
-                        }
-                    ]
-                },
-                {
-                    "title": "云安全",
-                    "icon": "safe",
-                    'href': '/console/security/basic',
-                    "subNav": [
-                        {
-                            "title": "基础防护",
-                            "href": "/console/security/basic",
-                            "index": "security-basic"
-                        },
-                        {
-                            "title": "高防IP",
-                            "href": "/console/security/kad",
-                            "index": "security-kad"
                         }
                     ]
                 }
