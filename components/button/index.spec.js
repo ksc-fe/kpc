32fd0afd0b844f4fee6a603149dcc759c499e09d
dudaodong <dudaodong@kingsoft.com>
2017-10-13 11:32:31 +0800

    update

--- components/button/index.spec.js
+++ components/button/index.spec.js
@@ -1,5 +1,6 @@
 import Intact from 'intact';
 import Button from './index';
+import App from '../app/index';
 
 /**
  * @desc 创建Intact组件实例
@@ -19,23 +20,25 @@ function getRenderedInstance (Component, propsData) {
 
 describe('Button unit test:', () => {
   
-  let buttonVM;
+  let button, app;
 
   afterEach(() => {
-      //buttonVM.$destroy(); //Intact未暴露$destroy
+      //button.destroy();
+      //document.body.removeChild(button.element);
   });
 
   //Button type属性
   it('should render correct primary type button', () => {
-    buttonVM = getRenderedInstance(Button, {
+    button = getRenderedInstance(Button, {
         type: "primary"
     });
-    // let type = buttonVM.get('type');
-    // expect(type).to.equal("primary");
-    buttonVM.init();
-    let buttonEle = buttonVM.element;
-    console.log('buttonEle', buttonEle);
-    expect(buttonEle.classList.contains('k-primary')).to.be.true;
+    let type = button.get('type');
+    expect(type).to.equal("primary");
+    // button.init();
+    // button.mount();
+    // let buttonEle = button.element;
+    // console.log('buttonEle', buttonEle);
+    // expect(buttonEle.classList.contains('k-primary')).to.be.true;
   });
 
 })
