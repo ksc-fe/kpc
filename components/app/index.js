0bdc885e8ad721d015e93243fc6ee8568768a9fd
Javey <jiawei23716@sina.com>
2017-10-15 13:23:20 +0800

    set app style

--- components/app/index.js
+++ components/app/index.js
@@ -1,6 +1,7 @@
 import Intact from 'intact';
 import template from './index.vdt';
 import serverStyleCleanup from 'node-style-loader/clientCleanup';
+import './index.styl';
 
 export default class extends Intact {
     @Intact.template()
