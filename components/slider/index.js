69e409f8ac68522c86dbdc7b9f35535700fc607d
lijia <lijia8@kingsoft.com>
2017-10-24 17:36:39 +0800

    lijia

--- components/slider/index.js
+++ components/slider/index.js
@@ -12,11 +12,40 @@ export default class extends Intact{
             size: 'default',// default | small | mini
             max: 100,
             min: 0,
-            value: 30,
-            isRange: false,
+            value: [20, 50],
+            isRange: true,
             unit: '',
             isShowEnd: true
         }
     }
+
+    clickWrapper(e) {
+        if(this.get('disabled')) return;
+        let currentPosition = e.clientX,
+            boundingPostion = this.$slider.getBoundingClientRect().left,
+            percent = (currentPosition - boundingPostion) / this.$slider.offsetWidth,
+            sliderValue = this.get('max') - this.get('min'),
+            value;
+            if(percent <= 0 ) {
+                value = this.get ('min')
+            } else if (percent >= 1) {
+                value = this.get ('max')
+            } else {
+                value = sliderValue * percent;
+            }
+        if(!this.get('isRange')) {
+                this.set('value', value);
+        } else {
+            let leftBtnPosition = this.$sliderFirstBtn.getBoundingClientRect().left,
+                rightBtnPosition = this.$sliderSecondBtn.getBoundingClientRect().left,
+                valueArr = this.get('value');
+            Math.abs(leftBtnPosition - currentPosition) <= Math.abs(rightBtnPosition -currentPosition)   ? valueArr[0] = value: valueArr[1] = value;
+            this.set('value', valueArr);
+            this.update();
+        }
+
+
+    }
+
 
 }
