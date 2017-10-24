8a58e2d29220042eb75b1fb13ad469cd89842694
lijia <lijia8@kingsoft.com>
2017-10-24 15:03:22 +0800

    lijia

--- components/slider/index.js
+++ components/slider/index.js
@@ -2,8 +2,21 @@
  * Created by sylvia on 2017/10/18.
  */
 import template from './index.vdt'
+import './index.styl'
 
 export default class extends Intact{
     get template() { return template;}
+
+    defaults() {
+        return {
+            size: 'default',// default | small | mini
+            max: 100,
+            min: 0,
+            value: 30,
+            isRange: false,
+            unit: '',
+            isShowEnd: true
+        }
+    }
 
 }
--- pages/slider/index.js
+++ pages/slider/index.js
@@ -2,12 +2,14 @@
  * Created by sylvia on 2017/10/18.
  */
 import template from './index.vdt'
+import './index.styl'
 
 export default class extends Intact{
 
     get template() { return template;}
 
-    _init() {
-        console.log('test slider')
+    defaults() {
+        return {
+        }
     }
 }
