ff3dac3d2dd0efe9505a945f6222cb015bc8df52
zhangyun <zhangyunmed@gmail.com>
2017-10-31 11:12:40 +0800

    zy

--- core/router.js
+++ core/router.js
@@ -17,5 +17,66 @@ export default new Router([
             return {Page};
         }
     },
-    ...routes
+    {
+        path: '/components/button',
+        action: async () => {
+            const Page = await import('pages/button');
+            return {Page};
+        }
+    },
+    {
+        path: '/components/dialog',
+        action: async () => {
+            return {Page: await import('pages/dialog')};
+        }
+    },
+    {
+        path: '/components/table',
+        action: async () => {
+            return {Page: await import('pages/table')};
+        }
+    },
+    {
+        path: '/components/checkbox',
+        action: async () => {
+            return {Page: await import('pages/checkbox')};
+        }
+    },
+    {
+        path: '/components/progress',
+        action: async () => {
+            return {Page: await import('pages/progress')};
+        }
+    },
+    {
+        path: '/components/radio',
+        action: async () => {
+            return {Page: await import('pages/radio')};
+        }
+    },
+    {
+        path: '/components/select',
+        action: async () => {
+            return {Page: await import('pages/select')}
+        }
+    },        
+    {
+        path: '/components/slider',
+        action: async () => {
+            return {Page: await import('pages/slider')}
+        }
+    },
+    {
+        path: '/components/spinner',
+        action: async () => {
+            return {Page: await import('pages/spinner')}
+        }
+    },
+    {
+        path: '/components/pagination',
+        action: async () => {
+            return {Page: await import('pages/pagination')}
+        }
+    },
+
 ]);
