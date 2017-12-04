f116b5de8140a07ba811f2583381efbb59d79b31
Javey <jiawei23716@sina.com>
2017-12-04 18:24:37 +0800

    feat: editable component & vertical-algin

--- components/editable/index.js
+++ components/editable/index.js
@@ -10,13 +10,79 @@ export default class extends Intact {
         return {
             editing: false,
             value: undefined,
+            required: true,
             validate: undefined,
             disabled: false, 
             tip: '编辑',
+            trim: true,
+            invalid: false,
         };
+    }
+
+    _init() {
+        this.initValue = this.get('value');
     }
 
     _edit() {
         this.set('editing', true);
+        const input = this.refs.input;
+        if (input.select) {
+            input.select();
+        } else if (input.setSelectionRange) {
+            // mobile safari
+            input.focus();
+            input.setSelectionRange(0, this.get('value.length', 0));
+        }
+    }
+
+    _onBlur(e) {
+        this._setValue(e.target.value);
+    }
+
+    _onKeydown(e) {
+        switch (e.keyCode) {
+            case 27: // esc 
+                this.set('editing', false);
+                break;
+            case 13: // enter
+                this._onBlur(e);
+                break;
+        }
+    }
+
+    _setValue(value) {
+        const {validate, required, trim} = this.get();
+
+        if (trim) value = value.trim();
+        
+        let valid = true;
+
+        if (required && !value) {
+            valid = false;
+        } else if (validate && value) {
+            if (typeof validate === 'function') {
+                valid = validate.call(this, value);
+            } else if (validate instanceof RegExp) {
+                valid = validate.test(value);
+            } else if (typeof validate === 'string') {
+                valid = new RegExp(validate).test(value);
+            }
+        }
+
+        if (!valid) {
+            this.set('value', value, {silent: true});
+            this.set('invalid', true);
+            return this.trigger('error', this, value);
+        }
+
+        this.set({
+            invalid: false,
+            editing: false,
+            value: value
+        });
+    }
+
+    reset() {
+        this.set('value', this.initValue);
     }
 }
--- pages/editable/index.js
+++ pages/editable/index.js
@@ -2,4 +2,10 @@ import template from './index.vdt';
 
 export default class extends Intact {
     get template() { return template; }
+
+    defaults() {
+        return {
+            text: '可编辑文本'
+        }
+    }
 }
