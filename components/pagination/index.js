a28267b24b25cc0cb9595974c97f84ed1cf911f7
zhangyun <zhangyunmed@gmail.com>
2017-12-11 13:55:11 +0800

    zy

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -1,5 +1,6 @@
 import template from './index.vdt'
 import './index.styl'
+import Intact from 'intact'
 
 export default class extends Intact{
     get template() { return template; }
@@ -8,12 +9,73 @@ export default class extends Intact{
         return {
             total: 0,
             'page-size': 10,
-            current: 1
+            current: 1,
+            showTotal: true,
+            opts: [
+                {text:'10行', value: 10},
+                {text:'20行', value: 20},
+                {text:'50行', value: 50},
+            ],
             
         };
     }
 
     _init() {
+        console.log('aa=',this.get('opts'))
+        if (this.get('current') > this.get('total')) {
+            this.set('current', 1);
+        }
         
+    }
+
+    changePage(page) {
+        if (this.get('current') !== page) {
+            this.set('current', page);
+        }
+    }
+
+    prev() {
+        let current = this.get('current');
+        if (current <= 1) {
+            return false;
+        }
+        this.changePage(current - 1);
+    }
+
+    next() {
+        let current = this.get('current');
+        let total = this.get('total');
+        if (current >= total) {
+            return false;
+        }
+        this.changePage(current + 1);
+    }
+
+    fastPrev() {
+        const page = this.get('current') - 5;
+        if (page > 0) {
+            this.changePage(page);
+        } else {
+            this.changePage(1);
+        }
+    }
+
+    fastNext() {
+        const page = this.get('current') + 5;
+        let total = this.get('total');
+        if (page > total) {
+            this.changePage(total);
+        } else {
+            this.changePage(page);
+        }
+    }
+
+    onPage(current) {
+        this.changePage(current);
+    }
+
+    onSize(pageSize) {
+        //
+        this.changePage(1);
     }
 }
