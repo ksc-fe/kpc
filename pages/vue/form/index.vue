9f90ebc5723c027a6ece7ecf327804b02f9e5091
Javey <jiawei23716@sina.com>
2017-12-25 20:00:47 +0800

    feat: checkbox & radio use relative unit for size

--- pages/vue/form/index.vue
+++ pages/vue/form/index.vue
@@ -3,20 +3,18 @@
         @submit="submit"
     >
         <FormItem label="用户名" model="model.userName" :rules="{required: true, checkName: true}">
-            <input class="c-input" v-model="model.userName" />
+            <Input v-model="model.userName" />
         </FormItem>
         <FormItem label="姓名" class="real-user-name">
             <Row gutter="16">
                 <Col span="12">
                     <FormItem hideLabel model="model.realUserName.firstName" :rules="{required: model.realUserName.lastName}">
-                        <input 
-                            class="c-input" v-model="model.realUserName.firstName"
-                        />
+                        <Input v-model="model.realUserName.firstName" />
                     </FormItem>
                 </Col>
                 <Col span="12">
                     <FormItem hideLabel model="model.realUserName.lastName" :rules="{required: model.realUserName.firstName}">
-                        <input class="c-input" v-model="model.realUserName.lastName" />
+                        <Input v-model="model.realUserName.lastName" />
                     </FormItem>
                 </Col>
             </Row>
@@ -31,26 +29,26 @@
             <Checkbox v-model="model.hobbies" trueValue="coding" name="hobbies" style="margin-left: 10px;">写代码</Checkbox>
         </FormItem>
         <FormItem label="年龄" model="model.age" :rules="{required: true, digits: true, min: 1}">
-            <input class="c-input" v-model="model.age" />
+            <Input v-model="model.age" />
         </FormItem>
         <FormItem label="邮箱" model="model.email" :rules="{email: true}">
-            <input class="c-input" v-model="model.email" />
+            <Input v-model="model.email" />
         </FormItem>
         <FormItem label="密码" model="model.password" :rules="{required: true}">
-            <input class="c-input" type="password" v-model="model.password" />
+            <Input type="password" v-model="model.password" />
         </FormItem>
         <FormItem v-if="!hide" label="确认密码" model="model.confirmPassword" :rules="{required: true, equal(value) { return value === model.password || '两次密码输入不一致'; }}">
-            <input class="c-input" type="password" v-model="model.confirmPassword" />
+            <Input type="password" v-model="model.confirmPassword" />
         </FormItem>
         <FormItem label="描述" class="descriptions">
             <FormItem v-for="(value, key) in model.descriptions" :model="'model.descriptions.' + key" hideLabel :rules="{required: true}" :key="key">
-                <input class="c-input" v-model="model.descriptions[key]" />
+                <Input v-model="model.descriptions[key]" />
             </FormItem>
             <Button @click="add" size="small">添加</Button>
         </FormItem>
         <FormItem label="验证码" model="model.captcha" :rules="{isEqual: true}">
             <template slot="label">验证码 (<span style="color: red;">3</span>)</template>
-            <input class="c-input" v-model="model.captcha" />
+            <Input v-model="model.captcha" />
         </FormItem>
         <FormItem>
             <Button htmlType="submit" type="primary">提交</Button>
@@ -65,6 +63,7 @@ import Radio from 'components/radio';
 import Button from 'components/button';
 import Checkbox from 'components/checkbox';
 import {Row, Col} from 'components/grid';
+import Input from 'components/input';
 
 Form.addMethod('isEqual', function(value, item) {
     return this.optional(item) || value == 3;
@@ -79,7 +78,7 @@ Form.addMethod('checkName', function(value, item) {
 export default {
     components: {
         Form, FormItem, Radio, Button, Checkbox,
-        Row, Col
+        Row, Col, Input
     },
 
     data() {
