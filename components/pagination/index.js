08de81ca3c64f970308cf6432dbb1d077d8f9245
Javey <jiawei23716@sina.com>
2018-01-05 17:28:04 +0800

    upd: pagination component;

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -1,100 +1,69 @@
-import template from './index.vdt'
-import './index.styl'
-import Intact from 'intact'
+import Intact from 'intact';
+import template from './index.vdt';
+import './index.styl';
 
 export default class Pagination extends Intact {
-    get template() { return template; }
+    @Intact.template()
+    static template = template;
 
     defaults() {
         return {
             total: 0,
             current: 1,
-            size: 10,
-            sizeValue: 10,
+            limit: 10,
+            counts: 7,
             showTotal: true,
-            opts: [10,20,50],
+            limits: [10, 20, 50],
             value: '',
-            Goto: false
+            showGoto: false,
+            size: 'default',
         };
     }
 
     _init() {
-        if (this.get('current') > this.get('total')) {
-            this.set('current', 1);
-        }
+        // avoid setting incorrect value
+        this.changePage(this.get('current'));
 
-        this.on('$change:sizeValue', () => {
-            console.log(parseInt(this.get('sizeValue').replace('行','')))
+        this.on('$change:limit', () => {
             this.set('current', 1);
-            this.set('size', parseInt(this.get('sizeValue').replace('行','')))
-        })
+        });
     }
 
     changePage(page) {
-        if (this.get('current') !== page) {
-            this.set('current', page);
+        const {total, limit} = this.get();
+        const totalPages = Math.ceil(total / limit);
+
+        if (page > totalPages) {
+            page = totalPages;
         }
+        if (page < 1) {
+            page = 1;
+        }
+
+        this.set('current', page);
     }
 
     prev() {
-        let current = this.get('current');
-        if (current <= 1) {
-            return false;
-        }
-        this.changePage(current - 1);
+        this.changePage(this.get('current') - 1);
     }
 
     next() {
-        let current = this.get('current');
-        let total = this.get('total');
-        if (current >= total) {
-            return false;
-        }
-        this.changePage(current + 1);
+        this.changePage(this.get('current') + 1);
     }
 
     fastPrev() {
-        const page = this.get('current') - 5;
-        if (page > 0) {
-            this.changePage(page);
-        } else {
-            this.changePage(1);
-        }
+        const page = this.get('current') - (this.get('counts') - 4);
+        this.changePage(page);
     }
 
     fastNext() {
-        const page = this.get('current') + 5;
-        let total = this.get('total');
-        if (page > total) {
-            this.changePage(total);
-        } else {
-            this.changePage(page);
-        }
+        const page = this.get('current') + (this.get('counts') + 4);
+        this.changePage(page);
     }
 
-    _goto() {
-        const self = this;
-        const regu = /^[1-9]\d*$/;
-        let value = parseInt(self.get('value'));
-        let total = self.get('total');
-        let size = self.get('size');
-        let page = value;
-        let totalPages = Math.ceil(total / size)
-
-        if (regu.test(value)) {
-            if (value <= 1) {
-                page = 1;
-            } else if (value > totalPages) {
-                page = totalPages;
-            }
-            self.set('value', page);
-            self.changePage(page);
-        }
-    }
-
-    _changeSize() {
-        console.log(this.get('sizeValue'))
-        this.set('current', 1);
-        this.set('size',this.get('sizeValue'))
+    _goto(e) {
+        // const regexp = /^[1-9]\d*$/;
+        const value = parseInt(e.target.value);
+        this.changePage(value);
     }
 }
