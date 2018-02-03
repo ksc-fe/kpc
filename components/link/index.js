c60c5eeeb025a6a88c2bce209eed6f0af490883c
Javey <jiawei23716@sina.com>
2018-02-03 15:36:57 +0800

    feat: support name for link

--- components/link/index.js
+++ components/link/index.js
@@ -10,6 +10,7 @@ export default class Link extends Intact {
     defaults() {
         return {
             href: undefined,
+            name: undefined,
             isReplace: false,
         }
     }
@@ -18,16 +19,27 @@ export default class Link extends Intact {
         e.preventDefault();
         this.trigger('click', e);
         const href = this.get('href');
+        const name = this.get('name');
         const history = Link.history;
+        let url;
         if (href != null) {
+            url = href;
+        } else if (name != null) {
+            if (Array.isArray(name)) {
+                url = Link.url.apply(Link.url, name);
+            } else {
+                url = Link.url(name);
+            }
+        }
+        if (url) {
             // if the href is equal to the current href, then do nothing
             const {pathname, search} = history.location;
-            if (pathname + search === href) return;
+            if (pathname + search === url) return;
 
             if (this.get('isReplace')) {
-                history.replace(href);
+                history.replace(url);
             } else {
-                history.push(href);
+                history.push(url);
             }
         }
     }
