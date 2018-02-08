f73036ca321268a49f8109e5d014e39395e846fc
Javey <jiawei23716@sina.com>
2018-02-08 16:25:15 +0800

    revert

--- components/radio/index.js
+++ components/radio/index.js
@@ -26,12 +26,6 @@ export default class Radio extends Intact {
             this.set('value', undefined);
         }
     }
-
-    // @referenct https://stackoverflow.com/questions/24501497/why-the-onclick-element-will-trigger-twice-for-label-element
-    _onClick(e) {
-        e.preventDefault();
-        this.trigger('click', e);
-    }
 }
 
 export {Radio};
