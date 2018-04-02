4ee4a6eaaaa0f2f2aab1c955b20c130df4ee66d4
Javey <jiawei23716@sina.com>
2018-04-02 20:20:42 +0800

    chore: use require instead of exsits

--- styles/functions.js
+++ styles/functions.js
@@ -1,14 +1,24 @@
 const stylus = require('stylus');
 const path = require('path');
+const nodes = stylus.nodes;
 
 module.exports = function() {
     return function(style) {
-        style.define('exists', function(path) {
-            return !!stylus.utils.lookup(path.string, this.paths);
+        style.define('exists', function(file) {
+            return !!stylus.utils.lookup(file.string, this.paths);
         });
 
         style.define('__dirname', function() {
             return path.dirname(style.nodes.filename);
+        });
+
+        style.define('require', function(file) {
+            file = stylus.utils.lookup(file.string, this.paths);
+            if (file) {
+                const res = new nodes.Root();
+                res.push(new nodes.Import(new nodes.String(file)));
+                return res;
+            }
         });
     };
 };
