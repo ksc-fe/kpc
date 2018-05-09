eb13affa1670da552a4a35d3c01ebb540e0d386d
Javey <jiawei23716@sina.com>
2018-05-09 15:58:52 +0800

    build: 2018-05-09 15:58:51

--- components/table/demos/rowExpandable/index.js
+++ components/table/demos/rowExpandable/index.js
@@ -6,4 +6,21 @@ import './index.styl';
 export default class extends Intact {
     @Intact.template()
     static template = template;
+
+    defaults() {
+        return {
+            expandedKeys: []
+        };
+    }
+
+    toggleExpand(data, index) {
+        const expandedKeys = this.get('expandedKeys').slice(0);
+        const i = expandedKeys.indexOf(index);
+        if (i > -1) {
+            expandedKeys.splice(i, 1);
+        } else {
+            expandedKeys.push(index);
+        }
+        this.set('expandedKeys', expandedKeys);
+    }
 }
