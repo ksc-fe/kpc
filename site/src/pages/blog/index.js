a377698497efca1bc999d557c1b86bc2cef50b3a
Javey <jiawei23716@sina.com>
2018-06-05 17:45:35 +0800

    chore(doc): aside for blog

--- site/src/pages/blog/index.js
+++ site/src/pages/blog/index.js
@@ -6,4 +6,16 @@ export default class extends Document {
     @Intact.template()
     static template = template;
 
+    scrollToTop() {
+
+    }
+
+    scrollTo(id) {
+        const header = document.getElementById(id);
+        if (header) {
+            const top = header.getBoundingClientRect().top + window.pageYOffset - 80;
+
+            window.scrollTo(0, top);
+        }
+    }
 }
