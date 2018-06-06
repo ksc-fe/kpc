f315201d103b53c3769a734165ec57c52971f30e
x6doooo <x6doooo@gmail.com>
2018-06-06 11:02:45 +0800

    update: transfer支持全选和清空

---
+++ pages/vue/transfer/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <Transfer :data="data" :filter="filter" :filterable='true' :batchable='true'>
+        <div slot="label" slot-scope="data, index, type">
+            <div>{{ data.name }}</div>
+            <p>{{ data.desc }} | {{ data.ip }}</p>
+        </div>
+    </Transfer>
+</template>
+
+<script>
+import Transfer from 'components/transfer';
+
+export default {
+    data() {
+        return {
+            data: [
+                {name: '主机名0', desc: '前端服务器0', ip: '192.168.1.0'},
+                {name: '主机名1', desc: '前端服务器1', ip: '192.168.1.1'},
+                {name: '主机名2', desc: '前端服务器2', ip: '192.168.1.2'},
+                {name: '主机名3', desc: '前端服务器3', ip: '192.168.1.3', disabled: true},
+                {name: '主机名4', desc: '前端服务器4', ip: '192.168.1.4'},
+            ]
+        }
+    },
+
+    components: {Transfer},
+
+    methods: {
+        filter(data, keywords) {
+            return data.name.includes(keywords) || 
+                data.desc.includes(keywords) ||
+                data.ip.includes(keywords);
+        }
+    }
+}
+</script>
