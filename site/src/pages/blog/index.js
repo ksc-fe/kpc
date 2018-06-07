58bc2d48f2b50b3f4d2ba0834af9a99000c2f3b3
Javey <jiawei23716@sina.com>
2018-06-07 16:48:50 +0800

    chore: active aside for blog

--- site/src/pages/blog/index.js
+++ site/src/pages/blog/index.js
@@ -13,6 +13,11 @@ export default class extends Document {
 
     _mount() {
         super._mount();
+
+        this.h1s = this.element.querySelectorAll('h1');
+        this.h2s = this.element.querySelectorAll('h2');
+        this.h3s = this.element.querySelectorAll('h3');
+
         mermaid.init(undefined, '.mermaid');
     }
 
@@ -22,6 +27,42 @@ export default class extends Document {
             const top = header.getBoundingClientRect().top + window.pageYOffset - 80;
 
             window.scrollTo(0, top);
+        }
+    }
+
+    _onScroll() {
+        super._onScroll();
+
+        const scrollTop = window.pageYOffset;
+        
+        function findActive(hs, minTop = 0) {
+            for (let i = hs.length - 1; i >= 0; i--) {
+                const h = hs[i];
+                const top = h.getBoundingClientRect().top + window.pageYOffset;
+
+                if (top > minTop && top - 80 <= scrollTop) {
+                    return {header: h.id, top: top};
+                }
+            }
+            return {header: '', top: 0};
+        }
+
+        const active2 = findActive(this.h2s, 0);
+        const active3 = findActive(this.h3s, active2.top);
+        this.set({
+            'active2': active2.header,
+            'active3': active3.header, 
+        });
+
+        let active = this.refs.aside.querySelectorAll('.active');
+        active = active[active.length - 1];
+        if (active) {
+            this.set('borderStyle', {
+                height: active.offsetHeight + 'px',
+                top: active.offsetTop + 'px',
+            });
+        } else {
+            this.set('borderStyle', undefined);
         }
     }
 }
