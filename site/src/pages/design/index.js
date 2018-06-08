555311e760dd23d17c31b583d606caaf4ea6daca
Javey <jiawei23716@sina.com>
2018-06-08 18:14:04 +0800

    docs: remove stylus.min.js temporarily

--- site/src/pages/design/index.js
+++ site/src/pages/design/index.js
@@ -2,16 +2,16 @@ import Intact from 'intact';
 import Document from '../document';
 import template from './index.vdt';
 import './index.styl';
-import mixin from '!raw-loader!styles/mixin.styl';
+// import mixin from '!raw-loader!styles/mixin.styl';
 
 export default class extends Document {
     @Intact.template()
     static template = template;
 
-    _mount() {
-        console.log(stylus.render(`${mixin}
-.test
-    color palette(red, 1)
-`));
-    }
+    // _mount() {
+        // console.log(stylus.render(`${mixin}
+// .test
+    // color palette(red, 1)
+// `));
+    // }
 }
