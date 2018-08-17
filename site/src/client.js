7b37305785183a95535bfed9c8bacae273ca3e7d
Javey <jiawei23716@sina.com>
2018-08-17 19:31:14 +0800

    docs: update doc site

--- site/src/client.js
+++ site/src/client.js
@@ -3,9 +3,10 @@ import router from './router';
 import createHistory from 'history/createBrowserHistory';
 import App from 'kpc/components/app';
 import Link from 'kpc/components/link';
+import serverStyleCleanup from 'node-style-loader/clientCleanup';
 
 const history = createHistory();
-const $app = new App({container: document.getElementById('page')});
+const $app = new App({container: document.getElementById('page'), ssr: true});
 
 $app.history = history;
 Link.history = history;
@@ -16,7 +17,7 @@ function init(router) {
 
     unlisten = history.listen(async ({pathname}, action) => {
         const {Page, data} = await router.resolve({pathname});
-        $app.load(Page, data);
+        $app.load(Page, data, serverStyleCleanup);
     });
     history.replace(location);
 }
