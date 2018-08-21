d15de4fddd8d57279f52abca60ddd13e446fb22d
Javey <jiawei23716@sina.com>
2018-08-21 20:39:10 +0800

    feat(Spin): loading component, closes #74

---
+++ components/spin/index.js
@@ -0,0 +1,21 @@
+import Intact from 'intact';
+import template from './index.vdt';
+import '../../styles/kpc.styl';
+import './index.styl';
+
+export default class Spin extends Intact {
+    @Intact.template()
+    static template = template;
+
+    static propTypes = {
+        size: ['large', 'default', 'small', 'mini'],
+    };
+
+    defaults() {
+        return {
+            size: 'default',
+        };
+    }
+}
+
+export {Spin};
--- site/src/client.js
+++ site/src/client.js
@@ -10,14 +10,16 @@ const $app = new App({container: document.getElementById('page'), ssr: true});
 
 $app.history = history;
 Link.history = history;
+window.$app = $app;
 
 let unlisten;
 function init(router) {
     if (unlisten) unlisten();
 
     unlisten = history.listen(async ({pathname}, action) => {
+        $app.showLoading();
         const {Page, data} = await router.resolve({pathname});
-        $app.load(Page, data, serverStyleCleanup);
+        await $app.load(Page, data, serverStyleCleanup);
     });
     history.replace(location);
 }
