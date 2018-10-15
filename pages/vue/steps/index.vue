461b65a852b744f09a8dcd3ed31008675fd598de
Javey <jiawei23716@sina.com>
2018-10-15 19:54:11 +0800

    fix(Steps): can not divide step into equal parts, close #100

--- pages/vue/steps/index.vue
+++ pages/vue/steps/index.vue
@@ -1,12 +1,12 @@
 <template>
     <div style="margin: 20px">
         <Steps v-model="index">
-            <Step title="选择配置">请选择主机<br />的配置信息</Step>
+            <Step title="选择配置">请选择主机的配置信息</Step>
             <Step title="选择弹性IP">请选择主机弹性IP的配置信息</Step>
             <Step title="设置VPC" />
         </Steps>
         <Steps value="1" status="error" style="margin: 20px 0;">
-            <Step title="选择配置">请选择主机<br />的配置信息</Step>
+            <Step title="选择配置">请选择主机的配置信息</Step>
             <Step title="选择弹性IP">请选择主机弹性IP的配置信息</Step>
             <Step title="设置VPC">请选择主机对应的vpc</Step>
         </Steps>
