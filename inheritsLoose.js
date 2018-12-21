4b9a1801d083d69f84ed4c773ee57bbd95cf2d08
Javey <jiawei23716@sina.com>
2018-12-21 11:50:35 +0800

    chore: rename

---
+++ inheritsLoose.js
@@ -0,0 +1,11 @@
+var _Object$create = require("core-js/library/fn/object/create");
+var _defaults = require("@babel/runtime-corejs2/helpers/defaults");
+
+function _inheritsLoose(subClass, superClass) {
+  subClass.prototype = _Object$create(superClass.prototype);
+  subClass.prototype.constructor = subClass;
+  _defaults(subClass, superClass);
+  subClass.__proto__ = superClass;
+}
+
+module.exports = _inheritsLoose;
