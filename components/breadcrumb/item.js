cb39b47e61ee2c0ff9436ae7dfcec16dd44b5464
Javey <jiawei23716@sina.com>
2018-12-28 12:56:49 +0800

    feat(Breadcrumb): use separator block instead of property, close #165

--- components/breadcrumb/item.js
+++ components/breadcrumb/item.js
@@ -7,7 +7,7 @@ export default class BreadcrumbItem extends Intact {
 
     static propTypes = {
         to: String,
-        separator: String,
+        separator: [String, Intact.VNode]
     };
 
     defaults() {
