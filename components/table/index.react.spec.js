2c0fd0ccec0d916f899199758e23b39c5153235a
Javey <jiawei23716@sina.com>
2019-05-19 14:41:56 +0800

    test(Table): add react unit test

---
+++ components/table/index.react.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {mount, unmount} from '../../test/utils';
+import {Table, TableColumn} from 'kpc/components/table';
+import {Switch} from 'kpc/components/switch';
+
+describe('Table', () => {
+    it('render component in template', () => {
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+
+        ReactDOM.render(
+            <Table data={[{}]}>
+                <TableColumn
+                    key="a"
+                    title="test"
+                    template={(data) => {
+                        return <div>
+                            <Switch />
+                        </div>
+                    }}
+                />
+            </Table>,
+            container
+        );
+
+        // document.body.removeChild(container);
+    });
+});
