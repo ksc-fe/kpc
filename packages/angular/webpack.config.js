d25a407922088c05471fc9163ebc8474267e179d
Javey <jiawei23716@sina.com>
2019-09-16 20:06:55 +0800

    chore: update webpack config

--- packages/angular/webpack.config.js
+++ packages/angular/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = function(config) {
     // alias intact
-    config.resolve.alias.intact$ = 'intact-angular';
+    config.resolve.alias.intact$ = path.resolve(__dirname, './node_modules/intact-angular/dist/index.js');
+    config.resolve.alias.kpc = path.resolve(__dirname, '../../@stylus');
     // add loader for style
     // config.module.rules.find(rule => {
         // if (rule.test.toString() === '/\\.css$/') {
@@ -12,13 +13,13 @@ module.exports = function(config) {
     // });
     config.module.rules.find(rule => {
         if (rule.test.toString() === '/\\.styl$/') {
-            rule.exclude.push(path.resolve(__dirname, 'kpc'));
+            rule.exclude.push(path.resolve(__dirname, '../../@stylus'));
             rule.use.find(loader => {
                 if (loader.loader === 'stylus-loader') {
                     Object.assign(loader.options, {
                         'resolve url': true,
                         'include css': true,
-                        'import': path.resolve(__dirname, 'kpc/@stylus/styles/themes/ksyun/index.styl')
+                        'import': path.resolve(__dirname, '../../@stylus/styles/themes/ksyun/index.styl')
                     });
                     return true;
                 }
