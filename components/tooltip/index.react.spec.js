78c27816482325faf4810be89b755cc089e525a3
Javey <jiawei23716@sina.com>
2019-10-17 16:51:21 +0800

    chore: add unit test and add core-js as dep

--- components/tooltip/index.react.spec.js
+++ components/tooltip/index.react.spec.js
@@ -2,13 +2,24 @@ import {configure} from 'kpc/components/utils';
 import {Tooltip} from 'kpc/components/tooltip';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {mount, unmount} from '../../test/utils';
+import {mount, unmount, getElement} from '../../test/utils';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 describe('Tooltip', () => {
+    let container;
+
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => {
+        ReactDOM.render(null, container);
+    });
+
     it('should wrap Tooltip when wrapDropdown is true', () => {
         configure({useWrapper: true});
-        const container = document.createElement('div');
-        document.body.appendChild(container);
 
         ReactDOM.render(
             <Tooltip content="content">
@@ -20,6 +31,25 @@ describe('Tooltip', () => {
         expect(container.innerHTML).to.matchSnapshot();
 
         configure({useWrapper: false});
-        document.body.removeChild(container);
+    });
+
+    it('should show tooltip that nests in Provider of react-redux', () => {
+        const store = createStore(() => {});
+
+        let dom;
+        ReactDOM.render(
+             <Provider store={store}>
+                <div>
+                    <Tooltip content="hello">
+                        <i ref={i => dom = i}>test</i>
+                    </Tooltip>
+                </div>
+            </Provider>,
+            container
+        );
+        dom.click();
+
+        const tooltip = getElement('.k-tooltip-content');
+        expect(tooltip == null).to.be.false;
     });
 });
