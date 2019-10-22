e589828b1bca47ed53f8136f718860b23ccab183
Javey <jiawei23716@sina.com>
2019-10-22 20:35:55 +0800

    test: for test angular headless

--- packages/kpc-angular/karma.conf.js
+++ packages/kpc-angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [process.env.CI ? 'ChromeHeadless' : 'Chrome'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
