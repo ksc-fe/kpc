31a7b4af170cae1c11836a331b0ab3900547c233
Javey <jiawei23716@sina.com>
2019-12-13 14:42:51 +0800

    chore: build

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * kpc v1.3.0-beta.4
+ * kpc v1.3.0-beta.5
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -202,7 +202,7 @@ const components = {
     Upload
 };
 
-export const version = '1.3.0-beta.4';
+export const version = '1.3.0-beta.5';
 
 /* generate end */
 
