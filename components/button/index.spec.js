490466b197424fcbe379b483c50dc1b6de82611e
Javey <jiawei23716@sina.com>
2019-12-20 15:03:12 +0800

    test(Button): update unit test

--- components/button/index.spec.js
+++ components/button/index.spec.js
@@ -1,12 +1,12 @@
 import Button from 'kpc/components/button';
-import {dispatchEvent, mount, unmount} from 'test/utils';
+import {dispatchEvent, mount, unmount, wait} from 'test/utils';
 import GroupDemo from '~/components/button/demos/group';
 
 describe('Button', () => {
     let instance;
 
     afterEach(() => {
-        unmount(instance);
+        // unmount(instance);
     });
 
     it('should change value when click radio buttons', () => {
@@ -18,6 +18,11 @@ describe('Button', () => {
         expect(instance.get('cities')).to.eql(['shanghai']);
         dispatchEvent(instance.refs.__checkboxShanghai.element, 'click');
         expect(instance.get('cities')).to.eql([]);
+
+        // should set value correctly, even if the type of value is not an array
+        instance.set('cities', '');
+        dispatchEvent(instance.refs.__checkboxShanghai.element, 'click');
+        expect(instance.get('cities')).to.eql(['shanghai']);
     });
 
     it('method test', () => {
@@ -31,5 +36,18 @@ describe('Button', () => {
         expect(instance.element.outerHTML).to.matchSnapshot();
         instance.enable();
         expect(instance.element.outerHTML).to.matchSnapshot();
+    });
+
+    it('should blur on mouse up', async () => {
+        instance = mount(Button);
+
+        const el = instance.element;
+        el.focus();
+        // test color of focus style 
+        await wait(500); // has transition
+        expect(getComputedStyle(el).color).to.eql('rgb(10, 94, 238)');
+        dispatchEvent(el, 'mouseup');
+        await wait(500);
+        expect(getComputedStyle(el).color).to.eql('rgb(89, 89, 89)');
     });
 });
