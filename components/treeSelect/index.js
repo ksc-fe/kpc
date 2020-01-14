4ef13c7f0fc891e89626d235c796b683f3da9a83
Javey <jiawei23716@sina.com>
2020-01-14 17:25:43 +0800

    feat(TreeSelect): checkable, #423

--- components/treeSelect/index.js
+++ components/treeSelect/index.js
@@ -3,6 +3,7 @@ import template from './index.vdt';
 import Select from '../select';
 import '../../styles/kpc.styl';
 import './index.styl';
+import {toggleArray} from '../utils';
 
 export default class TreeSelect extends Select {
     @Intact.template()
@@ -28,26 +29,41 @@ export default class TreeSelect extends Select {
     _init() {
         super._init();
 
-        const resetMultiple = () => {
-            if (this.get('checkbox')) {
-                this.set('multiple', true, {silent: true});
+        // const resetMultiple = () => {
+            // if (this.get('checkbox')) {
+                // this.set('multiple', true, {silent: true});
+            // }
+        // };
+        // resetMultiple();
+        // this.on('$change:checkbox', resetMultiple);
+
+        this.on('$receive', (c, keys) => {
+            const {checkbox, multiple} = this.get();
+            if (keys.indexOf('checkbox') > -1) {
+                if (checkbox) {
+                    this.set('multiple', true, {silent: true});
+                } else if (keys.indexOf('multiple') < 0) {
+                    this.set('multiple', false, {silent: true});
+                }
+            } else {
+                if (keys.indexOf('multiple') < 0) {
+                    this.set('multiple', false, {silent: true});
+                }
             }
-        };
-        resetMultiple();
-        this.on('$change:checkbox', resetMultiple);
+        });
 
         this._initLabels();
         this.on('$change:value', this._initLabels);
-        this.on('$change:value', (c, v) => {
-            const {multiple, checkbox} = this.get();
-            if (!multiple && !checkbox) {
-                v = [v];
-            }
-            this.set({
-                '_selectedKeys': v,
-                '_checkedKeys': v,
-            }, {async: true});
-        });
+        // this.on('$change:value', (c, v) => {
+            // const {multiple, checkbox} = this.get();
+            // if (!multiple && !checkbox) {
+                // v = [v];
+            // }
+            // this.set({
+                // '_selectedKeys': v,
+                // '_checkedKeys': v,
+            // }, {async: true});
+        // });
     }
 
     _initLabels() {
@@ -84,11 +100,45 @@ export default class TreeSelect extends Select {
         loop(data);
     }
 
-    _select(node) {
-        this._onSelect(node.key);
-        if (!this.get('multiple')) {
-            this.set({_selectedKeys: [node.key]}, {async: true});
+    _select(node, e) {
+        const {multiple, checkbox, _checkedKeys} = this.get();
+        const key = node.key;
+        if (!checkbox) {
+            this._onSelect(key);
+            if (!multiple) {
+                this.set({_selectedKeys: [key]}, {async: true});
+            }
+        } else {
+            // check on click
+            this.refs.tree._toggleCheck(node, e);
         }
+    }
+
+    _onCheckedKeysChange(c, keys) {
+        const value = this._getAllCheckedKeys();
+        this.set({
+            _checkedKeys: keys,
+            value,
+        });
+    }
+
+    _getAllCheckedKeys() {
+        const keys = [];
+        const loop = (children) => {
+            for (let i = 0; i < children.length; i++) {
+                const node = children[i];
+                if (node.checked) {
+                    keys.push(node.key);
+                    continue;
+                }
+                if (node.children) {
+                    loop(node.children);
+                }
+            }
+        }
+        loop(this.refs.tree.root.children);
+
+        return keys;
     }
 }
 
