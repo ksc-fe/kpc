df5001bc78c978bfe10682122702c4cab2c34105
Javey <jiawei23716@sina.com>
2020-04-20 18:30:54 +0800

    chore: build

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * kpc v1.3.7
+ * kpc v1.3.8
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -205,7 +205,7 @@ const components = {
     Upload
 };
 
-export const version = '1.3.7';
+export const version = '1.3.8';
 
 /* generate end */
 
