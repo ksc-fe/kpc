4fcec4995e20e1409cb07b2932a43e81d6e31588
Javey <jiawei23716@sina.com>
2020-11-17 15:50:20 +0800

    chore: build [skip ci]

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * kpc v1.4.5
+ * kpc v1.4.6
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -205,7 +205,7 @@ const components = {
     Upload
 };
 
-export const version = '1.4.5';
+export const version = '1.4.6';
 
 /* generate end */
 
