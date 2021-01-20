892794d0e0f548b51b7ed2e390038dd3678da4a2
Javey <jiawei23716@sina.com>
2021-01-20 15:16:40 +0800

    chore: build task for kpc-vue-next

--- build/gulp/frameworks.js
+++ build/gulp/frameworks.js
@@ -8,8 +8,10 @@ function prepare() {
     const entries = ['components', 'i18n', 'styles', 'index.js', 'inheritsLoose.js', '@stylus'];
 
     return Promise.all([
-        ...['vue', 'react'].reduce((memo, type) => {
-            memo.push(rm(resolve(`./@${type}`)));
+        ...['vue', 'react', 'vue-next'].reduce((memo, type) => {
+            if (type !== 'vue-next') {
+                memo.push(rm(resolve(`./@${type}`)));
+            }
             entries.forEach(entry => {
                 memo.push(rm(resolve(`./packages/kpc-${type}/${entry}`)));
             });
@@ -28,24 +30,30 @@ function generateCopy(type) {
             .pipe(tap(file => {
                 if (path.extname(file.path) === '.js') {
                     const contents = file.contents.toString('utf-8');
-                    file.contents = Buffer.from(contents.replace(/['"]intact["']/, `'intact-${type}'`));
+                    const intact = type === 'vue-next' ? 'intact-vue' : 'intact-' + type;
+                    file.contents = Buffer.from(contents.replace(/['"]intact["']/, `'${intact}'`));
                 }
             }));
 
-        if (type !== 'angular') {
+        if (type !== 'angular' && type !== 'vue-next') {
+            // this will change file.path
             ret = ret.pipe(gulp.dest(resolve(`./@${type}`)));
         }
 
         return ret
             .pipe(tap(file => {
-                if (type !== 'angular' && /@css/.test(file.path)) {
-                    file.base = resolve(`./@${type}/@css`)
+                if (/@css/.test(file.path)) {
+                    if (type === 'vue-next') {
+                        file.base = resolve('./@css');
+                    } else if (type !== 'angular') {
+                        file.base = resolve(`./@${type}/@css`);
+                    }
                 }
             }))
             .pipe(gulp.dest(resolve(`./packages/kpc-${type}`)));
     });
 }
 
-['vue', 'react', 'angular'].forEach(type => generateCopy(type));
+['vue', 'react', 'angular', 'vue-next'].forEach(type => generateCopy(type));
 
 gulp.task('build@frameworks', gulp.series(prepare, gulp.parallel(...tasks, 'build@angular')));
