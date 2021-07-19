ce1b390b230c883fed7eba9e0e1c048a0ae8d51c
Javey <jiawei23716@sina.com>
2021-07-19 15:36:48 +0800

    refactor(Table): fix columns #631

--- components/table/table.ts
+++ components/table/table.ts
@@ -1,6 +1,7 @@
 import {Component, TypeDefs} from 'intact';
 import template from './table.vdt';
 import {useColumns} from './useColumns';
+import {useFixedColumns} from './useFixedColumns';
 
 export interface TableProps {
     data?: any[]
@@ -11,4 +12,5 @@ export class Table extends Component<TableProps> {
     static template = template;
 
     private columns = useColumns();
+    private fixedColumns = useFixedColumns(this.columns.getColumns);
 }
--- components/table/useColumns.ts
+++ components/table/useColumns.ts
@@ -1,11 +1,10 @@
 import {useInstance, VNodeComponentClass, Props, directClone} from 'intact';
 import {eachChildren, isComponentVNode} from '../utils';
 import {TableColumn, TableColumnProps} from './column';
-import {cx} from '@emotion/css';
+import {useState} from '../../hooks/useState';
 
 export function useColumns() {
     const instance = useInstance()!;
-    let schemes: Props<TableColumnProps>[] = [];
     let columns: VNodeComponentClass<TableColumn>[] = [];
 
     instance.on('$receive:children', handleChildren);
@@ -13,55 +12,14 @@ export function useColumns() {
     function handleChildren() {
         const children = instance.get('children');
 
-        schemes = [];
         columns = [];
 
-        let offset = 0;
-        let index = -1;
-        let lastLeftFixedIndex = -1;
-        let firstRightFixedIndex = -1;
         eachChildren(children, vNode => {
             if (isComponentVNode(vNode, TableColumn)) {
-                index++;
                 const clonedVNode = directClone(vNode) as VNodeComponentClass<TableColumn>;
-                const props = vNode.props!;
-                const scheme = {
-                    ...props,
-                    shadow: false,
-                    offset: 0,
-                };
-                clonedVNode.props = scheme;
                 columns.push(clonedVNode);
-                schemes.push(scheme);
-
-                const fixed = props.fixed;
-                if (fixed === 'left') {
-                    scheme.offset = offset;
-                    offset += props.width as number; 
-                    lastLeftFixedIndex = index;
-                } else if (fixed === 'right') {
-                    scheme.shadow = true;
-                    firstRightFixedIndex = index;
-                }
             }
         });
-
-        // mark the last fixed left column as shadow
-        if (lastLeftFixedIndex > -1) {
-            schemes[lastLeftFixedIndex].shadow = true;
-        }
-
-        // calculate the offset of fixed right columns
-        if (firstRightFixedIndex > -1) {
-            let offset = 0;
-            for (let i = schemes.length - 1; i >= firstRightFixedIndex; i--) {
-                const scheme = schemes[i];
-                if (scheme.fixed === 'right') {
-                    scheme.offset = offset;
-                    offset += scheme.width as number;
-                }
-            }
-        }
     }
 
     function onSort() {
@@ -72,21 +30,9 @@ export function useColumns() {
 
     }
 
-    return () => {
-        return {
-            schemes,
-            columns,
-        }
+    function getColumns() {
+        return columns;
     }
-}
 
-export function getClassAndStyleForFixed({className, fixed, shadow, offset}: Props<TableColumnProps>) {
-    return {
-        className: cx({
-            [className as string]: !!className,
-            [`k-fixed-${fixed}`]: !!fixed,
-            'k-shadow': shadow,
-        }),
-        style: fixed ? {[fixed]: `${offset}px`} : null,
-    };
+    return {getColumns} 
 }
