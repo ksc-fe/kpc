7f2937479cae91193955d9d594101f765c71cb74
gaofeixue1 <gaofeixue1@kingsoft.com>
2021-08-13 19:23:27 +0800

    refactor(Timepicker): 优化代码 #635

--- components/timeline/item.ts
+++ components/timeline/item.ts
@@ -1,15 +1,16 @@
 import {Component, TypeDefs} from 'intact';
 import template from './item.vdt';
-import {Sizes} from '../types';
+import {Sizes, sizes} from '../../styles/utils';
+import {Types, types} from './styles';
 
 export interface TimelineItemProps {
-    type?: 'primary' | 'success' | 'danger' | 'warning'
+    type?: Types
     size?: Sizes
 }
 
 const typeDefs: Required<TypeDefs<TimelineItemProps>> = {
-    type: ['primary', 'success', 'danger', 'warning'],
-    size: ['large', 'default', 'small', 'mini'],
+    type: [...types],
+    size: sizes,
 };
 
 const defaults = (): Partial<TimelineItemProps> => ({
