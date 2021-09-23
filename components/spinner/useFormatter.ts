286526f106f2d3a7bac3a7cfc15796e6567dc679
Javey <jiawei23716@sina.com>
2021-09-23 16:10:43 +0800

    refactor(Spinner): use hooks, #627, #660

---
+++ components/spinner/useFormatter.ts
@@ -0,0 +1,35 @@
+import {useInstance} from 'intact';
+import type {Spinner} from './';
+
+export function useFormatter() {
+    const instance = useInstance() as Spinner;
+
+    function format(value: number): string {
+        const {formatter, prefix, suffix} = instance.get();
+
+        if (!formatter) {
+            return `${prefix || ''}${value}${suffix || ''}`;
+        }
+
+        return formatter(value);
+    } 
+
+    function parse(value: string): number | typeof NaN {
+        const {parser, prefix, suffix} = instance.get();
+        // value = String(value);
+
+        if (!parser) {
+            if (prefix) {
+                value = value.replace(new RegExp(`^${prefix}`), '');
+            }
+            if (suffix) {
+                value = value.replace(new RegExp(`${suffix}$`), '');
+            }
+            return +value;
+        }
+
+        return parser(value);
+    }
+
+    return {format, parse};
+}
