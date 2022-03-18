52f297a9d765f2a118e0dbeb711b4f6b69cc671a
Javey <jiawei23716@sina.com>
2022-03-18 14:24:38 +0800

    test: force chromeheadless scale factor to equal 1

--- packages/kpc-vue/__tests__/karma.conf.js
+++ packages/kpc-vue/__tests__/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
         customLaunchers: {
             'MyChromeHeadless': {
                 base: 'ChromeHeadless',
-                flags: ['--window-size=1920,1080'],
+                flags: ['--window-size=1920,1080', '--force-device-scale-factor=1'],
             }
         },
         frameworks: ['webpack', 'mocha', 'sinon-chai', 'snapshot', 'mocha-snapshot'],
