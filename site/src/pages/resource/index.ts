aa059e8d4fb4c9f3a6d7f4d6a204c06fda384063
Javey <jiawei23716@sina.com>
2022-04-28 11:24:56 +0800

    site: update some links

--- site/src/pages/resource/index.ts
+++ site/src/pages/resource/index.ts
@@ -11,7 +11,7 @@ export const getSketch = (): void => {
 };
 
 export const getFigma = (): void => {
-    location.href = 'https://www.figma.com/file/Uo5AKEmBXT4HOD41CDWliK/King-Design-V1.0.0?node-id=0%3A1';
+    window.open('https://www.figma.com/file/Uo5AKEmBXT4HOD41CDWliK/King-Design-V1.0.0?node-id=0%3A1');
 };
 
 export const getWeights = (): void => {
