e6f4a2124f0d16e3ce3f0af8459af1d65a935c0e
MAZEWEI <mazewei@kingsoft.com>
2022-07-21 15:47:06 +0800

    feat: color-process

--- site/src/pages/colorProcess/index.ts
+++ site/src/pages/colorProcess/index.ts
@@ -1,15 +1,32 @@
 import template from './index.vdt';
 import {Component} from 'intact';
 import Chroma from 'chroma-js';
+import {Message, Dialog} from 'kpc';
 
 interface ColorProcessProps {
-    
+    inputColor: string,
+    theme: string,
+    colorType: string,
+    pickerColor: string,
+    contrastMenu: boolean,
+    preColor: string,
+    bacColor: string
 }
 
 export default class Index extends Component<ColorProcessProps> {
     static template = template;
+
+    private color = ''
     
-    static defaults = (): Partial<ColorProcessProps> => ({});
+    static defaults = (): Partial<ColorProcessProps> => ({
+        inputColor: '#0091EA',
+        theme: 'light',
+        colorType: 'HEX',
+        pickerColor: '#0091EA',
+        contrastMenu: false,
+        preColor: '#fd0009',
+        bacColor: '#d1d1d1'
+    });
 
     _palette(color: string, level: number) {
         const [h, s, b] = Chroma(color).hsv();
@@ -25,5 +42,29 @@ export default class Index extends Component<ColorProcessProps> {
         else
             return color;
     }
+    _changeColor(e: InputEvent) {
+        const target = e.target as HTMLInputElement;
+        const value = target.value.trim();
+        if (/#[0-9A-Fa-f]{6}/i.test(value)) {
+            console.log(target.value)
+            this.set('inputColor', target.value);
+        } else {
+            Message.error('请输入hex格式颜色值');
+        }
+    }
+    _changeTheme(val: string) {
+        this.set('theme', val);
+    }
+    _changeColorType(val: string) {
+        this.set('colorType', val);
+    }
+    _changeContrastShow(val: boolean) {
+        this.set('contrastMenu', val);
+    }
+    _switchColor() {
+        let temp = this.get('preColor');
+        this.set('preColor', this.get('bacColor'));
+        this.set('bacColor', temp);
 
+    }
 }
--- site/src/pages/colorProcess/style.ts
+++ site/src/pages/colorProcess/style.ts
@@ -1,5 +1,365 @@
 import {css} from '@emotion/css';
 
-export function makeStyles() {
-    return css``;
+export function makeStyles(theme :string) {
+    if(theme=='light')
+        return css`
+            height: 100%;
+            .k-btn {
+                font-size: 16px;
+            }
+            .k-layout-wrapper {
+                height: 100%;
+            }
+            .k-layout-header {
+                background: white;
+                position: relative;
+                display: flex;
+                align-items: center;
+                font-family: Roboto;
+                font-weight: 500;
+                font-size: 12px;
+                justify-content: space-between;
+                & > div {
+                    flex-grow: 1;
+                }
+                & > div:last-child {
+                    display: flex;
+                    justify-content: flex-end;
+                }
+                .header-title {
+                    text-align: center;
+                    font-size: 30px;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    color: black;
+                    
+                }
+                .header-logo {
+                    display: flex;
+                    padding-left: 25px;
+                    img {
+                        width: 50px;
+                    }
+                }
+                .header-options {
+                    line-height: 70px;
+                    color: black;
+                    vertical-align: middle;
+                    & > div {
+                        margin-left: 10px;
+                    }
+                    .contrast {
+                        cursor: pointer;
+                        text-align: center;
+                    }
+                    .themeChange {
+                        background-color: white;
+                    }
+                    .themeChange:first-child{
+                        border-right: none;
+                    }
+                    .themeChange:last-child{
+                        border-left: none;
+                    }
+                    .checkout {
+                        background-color: blue;
+                        color: white;
+                    }
+                    
+                }
+            }
+            .cp-body {
+                .card-title {
+                    font-size: 40px;
+                }
+                .input {
+                    margin-top: 40px;
+                    margin-bottom: 20px;
+                }
+                .Color-RGB {
+                    // border: 1px solid black;
+                    display: flex;
+                    align-items: center;
+                    & > div: last-child {
+                        margin-left: 10px;
+                    }
+                }
+                .recomend {
+                    .recomendItem {
+                        margin-top: 40px;
+                        .recomendItem-title {
+                            font-size: 24px;
+                            margin-bottom: 20px;
+                        }
+                        .colorPelette {
+                            display: flex;
+                            align-items: center;
+                            & > div:nth-child(n+5) {
+                                color: white;
+                            }
+                            & > div:nth-child(5) {
+                                margin-right: 2px;
+                                height:50px !important;
+                                line-height: 50px !important;
+                            }
+                            .colorPeletteItem {
+                                flex-grow: 1;
+                                width: 100px;
+                                height: 46px;
+                                text-align: center;
+                                line-height: 46px;
+                                vertical-align: middle;
+                            }
+                        } 
+                    }
+                    
+                }
+                .colorPelette {
+                    display: flex;
+                    align-items: center;
+                    & > div:nth-child(n+5) {
+                        color: white;
+                    }
+                    & > div:nth-child(5) {
+                        margin-right: 2px;
+                        height:50px !important;
+                        line-height: 50px !important;
+                    }
+                    .colorPeletteItem {
+                        flex-grow: 1;
+                        width: 100px;
+                        height: 46px;
+                        text-align: center;
+                        line-height: 46px;
+                        vertical-align: middle;
+                    }
+                } 
+            }
+        `;
+    else {
+        return css`
+            height: 100%;
+            .k-btn {
+                font-size: 16px;
+            }
+            .k-layout-wrapper {
+                height: 100%;
+            }
+            .k-layout-header {
+                background: black;
+                position: relative;
+                display: flex;
+                align-items: center;
+                font-family: Roboto;
+                font-weight: 500;
+                font-size: 12px;
+                justify-content: space-between;
+                & > div {
+                    flex-grow: 1;
+                }
+                & > div:last-child {
+                    display: flex;
+                    justify-content: flex-end;
+                }
+                .header-title {
+                    text-align: center;
+                    font-size: 30px;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    color: white;
+                }
+                .header-logo {
+                    display: flex;
+                    padding-left: 25px;
+                    img {
+                        width: 50px;
+                    }
+                }
+                .header-options {
+                    line-height: 70px;
+                    color: white;
+                    vertical-align: middle;
+                    & > div {
+                        margin-left: 10px;
+                    }
+                    .contrast {
+                        cursor: pointer;
+                        text-align: center;
+                    }
+                    .themeChange {
+                        background-color: white;
+                    }
+                    .themeChange:first-child{
+                        border-right: none;
+                    }
+                    .themeChange:last-child{
+                        border-left: none;
+                    }
+                    .checkout {
+                        background-color: blue;
+                        color: white;
+                    }
+                    
+                }
+            }
+            .cp-body {
+                background: #373737;
+                .k-card {
+                    background: #373737;
+                }
+                .card-title {
+                    font-size: 40px;
+                    color: white;
+                }
+                .input {
+                    margin-top: 40px;
+                    margin-bottom: 20px;
+                }
+                .Color-RGB {
+                    // border: 1px solid black;
+                    display: flex;
+                    align-items: center;
+                    & > div: last-child {
+                        margin-left: 10px;
+                    }
+                }
+                .recomend {
+                    .recomendItem {
+                        margin-top: 40px;
+                        .recomendItem-title {
+                            color: white;
+                            font-size: 24px;
+                            margin-bottom: 20px;
+                        }
+                        .colorPelette {
+                            display: flex;
+                            align-items: center;
+                            & > div:nth-child(n+5) {
+                                color: white;
+                            }
+                            & > div:nth-child(5) {
+                                margin-right: 2px;
+                                height:50px !important;
+                                line-height: 50px !important;
+                            }
+                            .colorPeletteItem {
+                                flex-grow: 1;
+                                width: 100px;
+                                height: 46px;
+                                text-align: center;
+                                line-height: 46px;
+                                vertical-align: middle;
+                            }
+                        } 
+                    }
+                    
+                }
+                .colorPelette {
+                    display: flex;
+                    align-items: center;
+                    & > div:nth-child(n+5) {
+                        color: white;
+                    }
+                    & > div:nth-child(5) {
+                        margin-right: 2px;
+                        height:50px !important;
+                        line-height: 50px !important;
+                    }
+                    .colorPeletteItem {
+                        flex-grow: 1;
+                        width: 100px;
+                        height: 46px;
+                        text-align: center;
+                        line-height: 46px;
+                        vertical-align: middle;
+                    }
+                } 
+            }
+        `;
+    }
+}
+
+export function changeTheme(theme: string){
+    if(theme == 'light') {
+        return css`
+            .k-layout-header {
+                background-color: white !important;
+                .header-title {
+                    color: black;
+                }
+            }
+        `
+    } 
+    return css`
+        .k-layout-header {
+            background-color: white !important;
+            .header-title {
+                color: white;
+            }
+        }
+    `
+}
+
+
+
+export function DiaStyles() {
+    return css`
+        .k-dialog-body {
+            display: flex ;
+            
+            & > div: first-child {
+                width: 50%;
+                padding: 10px 5px;
+                & > div: first-child {
+                    font-size: 24px;
+                }
+                & > div: last-child {
+                    font-size: 18px;
+
+                }
+            }
+            & > div: last-child {
+                width: 50%;
+                .contrast-box > div {
+                    margin-left: 50px;
+                    margin-top: 20px;
+                }
+                .contrast-box {
+                    border: 1px solid gray;
+                    width: 100%;
+                    height: 300px;
+                    & > div: nth-child(n+2) {
+                        font-size: 12px;
+                        display: flex;
+                        justify-content: 
+                    }
+                }
+                .title {
+                    font-size: 20px;
+                }
+                .contrast-number {
+                    & > div: first-child {
+                        font-size: 30px;    
+                    }
+                    & > div: last-child {
+                        vertical-align: bottom;
+                        line-height: 45px;
+                        margin-left: 5px; 
+                    }
+                }
+                .choose-box {
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+            } 
+        }
+        .k-dialog-cancel {
+            display: none;
+        }
+    `
 }
