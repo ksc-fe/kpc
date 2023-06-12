b7605752892e8ff99944a97efc90313fbb65ce92
Javey <jiawei23716@sina.com>
2023-06-12 16:28:51 +0800

    chore: build [skip ci]

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * kpc v1.6.2
+ * kpc v1.6.3-beta.0
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -205,7 +205,7 @@ const components = {
     Upload
 };
 
-export const version = '1.6.2';
+export const version = '1.6.3-beta.0';
 
 /* generate end */
 
