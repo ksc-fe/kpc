8a6edce4def1fc43d5c611a57a17cbfb01ff66de
Javey <jiawei23716@sina.com>
2023-06-15 17:45:38 +0800

    test: add unit test

--- packages/kpc-react/__tests__/components/drawer.spec.tsx
+++ packages/kpc-react/__tests__/components/drawer.spec.tsx
@@ -1,6 +1,6 @@
 import React, {createRef, useState, useEffect} from 'react';
 import * as ReactDOM from 'react-dom';
-import {Drawer, Card, Button, Select, Option} from '../../';
+import {Drawer, Card, Button, Select, Option, Cascader} from '../../';
 import {Component} from 'intact-react';
 import {getElement, wait, dispatchEvent} from '../../../../test/utils';
 
@@ -102,5 +102,67 @@ describe('Drawer', () => {
 
         await wait();
         expect(document.querySelector<HTMLElement>('.k-dialog-body .k-select-menu')!.textContent).to.eql('1');
+    });
+
+    it('test', async () => {
+        function Test({value}: {value: boolean}) {
+            const [data, setData] = useState<any[]>([])
+
+            useEffect(() => {
+                if (!value) return;
+                setData([{
+                    value: 'beijing',
+                    label: '北京',
+                    children: [
+                        {
+                            value: 'haidian',
+                            label: '海淀区'
+                        },
+                        {
+                            value: 'chaoyang',
+                            label: '朝阳区'
+                        },
+                        {
+                            value: 'fengtai',
+                            label: '丰台区'
+                        }
+                    ]
+                }]);
+            }, [value]);
+
+            return  (
+                <Card>
+                    {value && (
+                        <Wrapper>
+                            <Cascader data={data} />
+                        </Wrapper>
+                    )}
+                </Card>
+            );
+        }
+
+        function Wrapper(props: any) {
+            return <div>
+                <Card><div className="test">a</div></Card>
+                <div>{props.children}</div>
+            </div>
+        }
+
+        function App() {
+            const [value, setValue] = useState(false);
+
+            return <>
+                <div onClick={() => setValue(true)} className="click">show</div>
+                <Test value={value} />
+            </>
+        }
+
+        ReactDOM.render(<App />, container);
+
+
+        const click = container.querySelector<HTMLElement>('.click')!;
+        click.click();
+        await wait();
+        expect(container.querySelector<HTMLElement>('.test')).to.be.exist;
     });
 });
