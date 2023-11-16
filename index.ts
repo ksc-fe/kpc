7d95388d449db85213fdb858e48e72e952084a99
Javey <jiawei23716@sina.com>
2023-11-16 18:12:55 +0800

    chore: build [skip ci]

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*!
- * @king-design v3.0.0
+ * @king-design v3.0.1
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -64,6 +64,6 @@ export * from './components/treeSelect';
 export * from './components/upload';
 export * from './components/wave';
 
-export const version = '3.0.0';
+export const version = '3.0.1';
 
 /* generate end */
