36021c251ecb0d2dbbf990abdd54148be4f6449c
zhangbing4 <zhangbing4@kingsoft.com>
2024-02-27 19:00:52 +0800

    chore: build [skip ci]

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*!
- * @king-design v3.1.6
+ * @king-design v3.2.0
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -65,6 +65,6 @@ export * from './components/treeSelect';
 export * from './components/upload';
 export * from './components/wave';
 
-export const version = '3.1.6';
+export const version = '3.2.0';
 
 /* generate end */
