96d4744da71daa5c485471c2efde4caa0ff0974d
Javey <jiawei23716@sina.com>
2024-06-04 11:16:08 +0800

    feat(font): load woff2 firstly for performance, close #1009

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -8,6 +8,7 @@ declare module "*.vdt" {
 
 declare module "*.eot" {}
 declare module "*.ttf" {}
+declare module "*.woff2" {}
 declare module "*.woff" {}
 declare module "*.svg" {}
 declare module "*.png" {}
