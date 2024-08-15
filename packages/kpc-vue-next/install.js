4befca64956c351a9175f667254272820e96323a
Javey <jiawei23716@sina.com>
2024-08-15 17:09:15 +0800

    fix: Vue warn on install, close #1031

--- packages/kpc-vue-next/install.js
+++ packages/kpc-vue-next/install.js
@@ -1,12 +1,17 @@
 import * as components from './index';
+import { isFunction } from 'intact-shared';
 
 export function install(Vue) {
     for (let key in components) {
+        // exclude the non-component properties
         // the name of component starts with an upper case char
+        // and the value is a function
         const charCode = key.charCodeAt(0);
-        if (charCode < 'A'.charCodeAt(0) && charCode > 'Z'.charCodeAt(0)) continue;
+        if (charCode < 'A'.charCodeAt(0) || charCode > 'Z'.charCodeAt(0)) continue;
 
         const component = components[key];
+        if (!isFunction(component)) continue;
+
         Vue.component(`K${key}`, component);
         // support call method like this.$message.success('test'), #88
         const obj = Vue.config.globalProperties;
