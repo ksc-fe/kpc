6285aaf785fa068d320424e4420eaf4605703a57
zhangbing4 <zhangbing4@kingsoft.com>
2025-08-14 21:26:52 +0800

    chore: build [skip ci]

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*!
- * @king-design v3.6.0-beta.0
+ * @king-design v3.6.0
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -71,6 +71,6 @@ export * from './components/view';
 export * from './components/virtualList';
 export * from './components/wave';
 
-export const version = '3.6.0-beta.0';
+export const version = '3.6.0';
 
 /* generate end */
