7ecaa6057ec41e6435f4273aaf505dca4ee3a0de
zoujiawei <zoujiawei@kingsoft.com>
2025-11-26 15:41:17 +0800

    chore: build [skip ci]

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 /*!
- * @king-design v3.6.2-beta.1
+ * @king-design v3.6.2
  *
  * Copyright (c) Kingsoft Cloud
  * Released under the MIT License
@@ -71,6 +71,6 @@ export * from './components/view';
 export * from './components/virtualList';
 export * from './components/wave';
 
-export const version = '3.6.2-beta.1';
+export const version = '3.6.2';
 
 /* generate end */
